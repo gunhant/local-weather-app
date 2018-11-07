6adb7daf37daf9441f8ad8aace1a0e729c8d927d
gunhant <34014347+gunhant@users.noreply.github.com>
2018-11-07 11:22:17 -0500

    changes to unit test files

--- src/app/weather/weather.service.spec.ts
+++ src/app/weather/weather.service.spec.ts
@@ -2,11 +2,12 @@ import { TestBed } from '@angular/core/testing';
 
 import { WeatherService } from './weather.service';
 
-describe('WeatherService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { WeatherServiceFake } from './weather.service.fake';
 
-  it('should be created', () => {
-    const service: WeatherService = TestBed.get(WeatherService);
-    expect(service).toBeTruthy();
-  });
+describe('WeatherService', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule]
+  }));
+
 });
