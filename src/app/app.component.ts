0f325b9f0df1ac3c0ddc8d5781df10e0e919da71
gunhant <34014347+gunhant@users.noreply.github.com>
2018-11-07 16:33:53 -0500

    UI Optimizations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  /* templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'] */
+  template: `
+    <div>
+      <mat-toolbar color="primary">
+        <span>LocalCast Weather</span>
+      </mat-toolbar>
+      <div style="text-align:center"><h1>Your city, your forecast, right now!</h1></div>
+      <div fxLayout="row">
+        <div fxFlex></div>
+        <div fxFlex="300px">
+          <mat-card>
+          <h2>Current Weather</h2>
+          <app-current-weather></app-current-weather>
+          </mat-card>
+        </div>
+        <div fxFlex></div>
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'local-weather-app';
+  /* title = 'local-weather-app'; */
 }
---
+++ src/app/material.module.ts
@@ -0,0 +1,14 @@
+import { MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule } from '@angular/material';
+import { NgModule } from '@angular/core';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule
+  ],
+  exports: [
+    MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule
+  ]
+})
+export class MaterialModule { }
